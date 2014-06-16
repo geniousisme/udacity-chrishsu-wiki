2d644430bcaca813312058710b70892cf4595f95
Chris Hsu Mac <chia-hao.hsu@aiesec.net>
2014-06-16 23:27:45 +0800

    fix some bugs for blog

--- udacity-chrishsu/main.py
+++ udacity-chrishsu/main.py
@@ -39,10 +39,19 @@ class Art(db.Model):
   art     = db.TextProperty( required = True )
   created = db.DateTimeProperty( auto_now_add = True )
 
+def blog_key(name = 'default'):
+  return db.Key.from_path('blogs', name)
+
 class Blog( db.Model ):
   subject = db.StringProperty( required = True )
   content = db.TextProperty( required = True )
   created = db.DateTimeProperty( auto_now_add = True )
+  last_modified = db.DateTimeProperty( auto_now = True )
+
+  def render( self ):
+    self._render_text = self.content.replace('\n', '<br>')
+    return render_str("post.html", p = self)
+
 
 class MainHandler( Handler ):
   def render_ascii(self, title = "", art = "", error = ""):
@@ -85,7 +94,7 @@ class NewPostHandler( Handler ):
 
   def post( self ):
     subject = self.request.get("subject")
-    content = self.request.get("content")
+    content = self.request.get("content").replace('\n', '<br>')
 
     if subject and content:
       newpost = Blog( subject = subject, content = content )
